950ab385164550b101fe567d2832d6fe8fdac95b
R C <robsc801@gmail.com>
2020-11-03 04:04:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray() {
+  var arrays [1];
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  
+}
