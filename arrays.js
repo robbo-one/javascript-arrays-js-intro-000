a293b2d0d7bc666bc0928ba4984f7f5f7225d459
R C <robsc801@gmail.com>
2020-11-03 05:03:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function accessElementInArray(array,index){
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array.shift()
 }
-removeElementFromBeginningOfArray(array){
- array.slice(1)
+function removeElementFromBeginningOfArray(array){
+ return array.slice(1)
 }
