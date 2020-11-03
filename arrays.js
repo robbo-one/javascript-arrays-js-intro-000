eeafad6682eec5fa2cef07e6aa852422877096ad
R C <robsc801@gmail.com>
2020-11-03 05:14:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,5 +31,5 @@ function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
 function  destructivelyAddElementToEndOfArray(array, element) {
-  return [...array, "element"]
+  return [...array, element]
 }
